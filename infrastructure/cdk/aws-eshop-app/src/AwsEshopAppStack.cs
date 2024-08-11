90c8f87ac80a44895f67dc29180235111361777e
Abhinav singh <singh.abhinav00@gmail.com>
2024-08-11 11:15:06 +0530

    react app to s3 bucket cdk project setup

---
+++ infrastructure/cdk/aws-eshop-app/src/AwsEshopAppStack.cs
@@ -0,0 +1,50 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.S3.Deployment;
+using Constructs;
+
+namespace AwsEshopApp
+{
+    public class AwsEshopAppStack : Stack
+    {
+        public AwsEshopAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            var env = new Amazon.CDK.Environment
+            {
+                Region = "us-east-1" // Replace with your desired region
+            };
+
+            var siteBucket = new Bucket(this, "aws-eshop-bucket", new BucketProps
+            {
+                BucketName = "eshoponcloud.xyz",
+                WebsiteIndexDocument = "index.html",
+                PublicReadAccess = true,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true,
+                BlockPublicAccess = new BlockPublicAccess(new BlockPublicAccessOptions())
+                {
+                    BlockPublicAcls = false,
+                    BlockPublicPolicy = false,
+                    IgnorePublicAcls = false,
+                    RestrictPublicBuckets = false,
+                }
+            });
+
+            siteBucket.AddToResourcePolicy(new PolicyStatement(new PolicyStatementProps
+            {
+                Effect = Effect.ALLOW,
+                Principals = new[] { new AnyPrincipal() },
+                Actions = new[] { "s3:GetObject" },
+                Resources = new[] { siteBucket.ArnForObjects("*") }
+            }));
+
+            // Deploy the React app to the S3 bucket
+            new BucketDeployment(this, "DeployReactApp", new BucketDeploymentProps
+            {
+                Sources = new[] { Source.Asset("../../../clients/aws-eshop-app/build") }, // Update this path
+                DestinationBucket = siteBucket
+            });
+        }
+    }
+}
