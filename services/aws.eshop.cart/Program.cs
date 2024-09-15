35ac52c5e91b02ba88a26a71857d9dd19e7a25df
Abhinav singh <singh.abhinav00@gmail.com>
2024-09-15 17:32:52 +0530

    docker file

--- services/aws.eshop.cart/Program.cs
+++ services/aws.eshop.cart/Program.cs
@@ -3,6 +3,10 @@ using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
